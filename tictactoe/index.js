d94304caac2f916a2befdc7b2191b70e6b738437
Yujen <maharjanyuzen@gmail.com>
2023-07-16 13:44:07 +0545

    project-completed

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,20 +1,64 @@
 let turn = "X";
-
+game=true;
+let move=0;
 const changeturn = () => {
     turn = turn === "X" ? "O" : "X";
 };
 
 const checkwin = () => {
-    // Add your logic to check for winning conditions here
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    for(i=0; i<wins.length; i++){
+        const[a,b,c]=wins[i];
+        const boxes=document.querySelectorAll('.board')
+        if(boxes[a].innerText===turn && boxes[b].innerText===turn && boxes[c].innerText===turn) {
+            const player_turn= document.querySelector('.player_turn');
+            player_turn.textContent= turn + "won";
+            game=false;
+            break;
+        }  
+    }
+    move++;
+    if (move === 9) {
+        const player_turn = document.querySelector('.player_turn');
+        player_turn.textContent = "Oops! It's a draw!";
+        game = false;
+    }
 };
-
 let boxes = document.querySelectorAll(".board");
 boxes.forEach(box => {
     box.addEventListener('click', (event) => {
-        if (event.target.innerText === ' ') {
+        if(!game){
+            return;
+        }
+        if (event.target.innerText === '') {
             event.target.innerText = turn;
+            checkwin();
             changeturn();
+        }
+        if (game===true){
+            const player_turn= document.querySelector('.player_turn');
+            player_turn.textContent= turn+ 'turn';
         }
     });
 });
-
+const reset = document.querySelector('button');
+reset.addEventListener('click',()=>{
+    const boxes=document.querySelectorAll('.board')
+    boxes.forEach(box=>{
+        box.innerHTML='';
+    })
+    const player_turn= document.querySelector('.player_turn');
+    player_turn.textContent= "Lets play!";
+    turn = "X";
+    game=true;
+    move=0;
+})
